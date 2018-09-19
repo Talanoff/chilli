d4ebf1df1d4f5a6e5779a6ded16d8ee907783e7f
Oleh Talanoff <talanov.o@gmail.com>
2018-09-19 12:09:35 +0300

    #

--- app/Http/Resources/CartResource.php
+++ app/Http/Resources/CartResource.php
@@ -16,8 +16,8 @@ class CartResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'product' => isset($this->product_id) ? new ProductResource($this->product) : null,
-//            'kit' => isset($this->kit_id) ? new KitResource($this->kit) : null,
+            'product' => new ProductResource($this->product()->first()),
+            'kit' => new KitResource($this->kit()->first()),
             'quantity' => $this->quantity,
             'amount' => ($this->product_id ? $this->product->computed_price : $this->kit->amount) * $this->quantity,
         ];
