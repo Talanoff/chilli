ac6ccaaa22b5cbc42e3d3b97fa76d615ab208d1d
Oleh Talanoff <talanov.o@gmail.com>
2018-11-13 18:22:26 +0200

    Bug fix

--- app/Mail/FastBuy.php
+++ app/Mail/FastBuy.php
@@ -39,6 +39,8 @@ class FastBuy extends Mailable
      */
     public function build()
     {
+
+        dd(config('app.email'));
         return $this
             ->to(config('app.email'))
             ->subject('Быстрый заказ')
